56347a22e88e151ac74779dfc797a59fa5bd0ac3
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-04-13 23:43:46 +0530

    inital

---
+++ 1d.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+
+    // int arr[] = {1,2,3,4};
+    int arr[5];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr [3] >> arr[4];
+    arr [2] = 20;
+    arr [3] += 10;
+    cout << arr[2] <<"\n" <<  arr[3];
+
+    return 0;
+}
