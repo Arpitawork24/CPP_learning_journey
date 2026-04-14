8bfd2c58f6a15b76131eea70731239ba98ac9868
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-04-14 18:52:11 +0530

    1 nd 2 patterns

---
+++ patterns/1/1_1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+// created function-
+void printn(int n){
+    for (int i = 0; i<n;i++){
+        for (int j = 0; j<n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main (){
+
+    int n;
+    cin >> n;
+    printn(n);
+
+    return 0;
+}
